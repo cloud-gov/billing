ab42a8e0c7f0fe94c39515983ac0f63542cd0436
James Hochadel <james.hochadel@gsa.gov>
2025-08-26 15:08:29 -0400

    Fix compile errors in tests

--- internal/usage/recorder/recorder_test.go
+++ internal/usage/recorder/recorder_test.go
@@ -74,13 +74,28 @@ func (s *stubQuerier) BulkCreateMeasurement(_ context.Context, arg db.BulkCreate
 	return nil
 }
 
-func (s *stubQuerier) CreateCFOrg(_ context.Context, arg db.CreateCFOrgParams) (db.CFOrg, error) {
+func (s *stubQuerier) AccountingEquation(_ context.Context) ([]string, error) {
 	panic("unimplemented")
 }
-func (s *stubQuerier) CreateCustomer(_ context.Context, arg db.CreateCustomerParams) (db.Customer, error) {
+func (s *stubQuerier) CreateCFOrg(_ context.Context, arg pgtype.UUID) (db.CFOrg, error) {
+	panic("unimplemented")
+}
+func (s *stubQuerier) CreateCustomer(_ context.Context, arg string) (int64, error) {
+	panic("unimplemented")
+}
+func (s *stubQuerier) CreateMeasurement(_ context.Context, arg db.CreateMeasurementParams) (db.Measurement, error) {
 	panic("unimplemented")
 }
 func (s *stubQuerier) CreateMeasurements(_ context.Context, arg []db.CreateMeasurementsParams) (int64, error) {
+	panic("unimplemented")
+}
+func (s *stubQuerier) CreateMeter(_ context.Context, name string) (string, error) {
+	panic("unimplemented")
+}
+func (s *stubQuerier) CreatePriceWithID(_ context.Context, arg db.CreatePriceWithIDParams) (db.Price, error) {
+	panic("unimplemented")
+}
+func (s *stubQuerier) CreateReadingWithID(_ context.Context, arg db.CreateReadingWithIDParams) (db.Reading, error) {
 	panic("unimplemented")
 }
 func (s *stubQuerier) CreateResourceKind(_ context.Context, arg db.CreateResourceKindParams) (db.ResourceKind, error) {
@@ -90,6 +105,12 @@ func (s *stubQuerier) CreateResources(_ context.Context, arg db.CreateResourcesP
 	panic("unimplemented")
 }
 func (s *stubQuerier) CreateTier(_ context.Context, arg db.CreateTierParams) (db.Tier, error) {
+	panic("unimplemented")
+}
+func (s *stubQuerier) CreateTransaction(_ context.Context, arg db.CreateTransactionParams) (db.Transaction, error) {
+	panic("unimplemented")
+}
+func (s *stubQuerier) CreateUniqueReading(_ context.Context, arg db.CreateUniqueReadingParams) (db.Reading, error) {
 	panic("unimplemented")
 }
 func (s *stubQuerier) DeleteCFOrg(_ context.Context, id pgtype.UUID) error {
@@ -119,6 +140,9 @@ func (s *stubQuerier) GetResourceKind(_ context.Context, arg db.GetResourceKindP
 func (s *stubQuerier) GetTier(_ context.Context, id int32) (db.Tier, error) {
 	panic("unimplemented")
 }
+func (s *stubQuerier) GetTransaction(_ context.Context, id int32) ([]db.Transaction, error) {
+	panic("unimplemented")
+}
 func (s *stubQuerier) ListCFOrgs(_ context.Context) ([]db.CFOrg, error) {
 	panic("unimplemented")
 }
@@ -134,16 +158,25 @@ func (s *stubQuerier) ListResources(_ context.Context) ([]db.Resource, error) {
 func (s *stubQuerier) ListTiers(_ context.Context) ([]db.Tier, error) {
 	panic("unimplemented")
 }
+func (s *stubQuerier) ListTransactions(_ context.Context) ([]db.Transaction, error) {
+	panic("unimplemented")
+}
+func (s *stubQuerier) ListTransactionsWide(_ context.Context) ([]db.ListTransactionsWideRow, error) {
+	panic("unimplemented")
+}
+func (s *stubQuerier) SumEntries(_ context.Context) ([]pgtype.Numeric, error) {
+	panic("unimplemented")
+}
 func (s *stubQuerier) UpdateCFOrg(_ context.Context, arg db.UpdateCFOrgParams) error {
 	panic("unimplemented")
 }
 func (s *stubQuerier) UpdateCustomer(_ context.Context, arg db.UpdateCustomerParams) error {
 	panic("unimplemented")
 }
-func (s *stubQuerier) UpdateResource(_ context.Context, arg db.UpdateResourceParams) error {
+func (s *stubQuerier) UpdateMeasurementMicrocredits(_ context.Context, asOf pgtype.Timestamptz) (pgtype.Int8, error) {
 	panic("unimplemented")
 }
-func (s *stubQuerier) UpdateResourceKind(_ context.Context, arg db.UpdateResourceKindParams) error {
+func (s *stubQuerier) UpdateResource(_ context.Context, arg db.UpdateResourceParams) error {
 	panic("unimplemented")
 }
 func (s *stubQuerier) UpdateTier(_ context.Context, arg db.UpdateTierParams) error {
@@ -284,7 +317,7 @@ func TestRecordReading(t *testing.T) {
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
 			stub := &stubQuerier{errOn: tc.errOn}
-			err := recorder.RecordReading(t.Context(), nullLogger, stub, tc.reading)
+			err := recorder.RecordReading(t.Context(), nullLogger, stub, tc.reading, false)
 
 			if tc.wantErr && err == nil {
 				t.Fatalf("expected error, got nil")
