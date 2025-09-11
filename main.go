0880ffd167a0f33771ad9927632891be8aa44d78
annakamienska-mercer <anna.mercer@gsa.gov>
2025-09-11 09:37:36 -0500

    initialize log level from config

--- main.go
+++ main.go
@@ -52,15 +52,14 @@ func run(ctx context.Context, out io.Writer) error {
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
 	defer cancel()
 
-	logger := slog.New(slog.NewJSONHandler(out, &slog.HandlerOptions{
-		Level: slog.LevelDebug,
-	}))
-
-	logger.Debug("run: initializing CF client")
 	c, err := config.New()
 	if err != nil {
 		return fmtErr(ErrBadConfig, err)
 	}
+	logger := slog.New(slog.NewJSONHandler(out, &slog.HandlerOptions{
+		Level: c.LogLevel,
+	}))
+	logger.Debug("run: initializing CF client")
 	cfconf, err := cfconfig.New(c.ApiUrl,
 		cfconfig.ClientCredentials(c.CFClientId, c.CFClientSecret))
 	if err != nil {
