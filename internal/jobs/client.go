3c36dafb6b72d357ca2ed4640157f5adb27ad443
James Hochadel <james.hochadel@gsa.gov>
2025-10-09 08:53:55 -0400

    Post usage on the 1st of every month at 6:01am
    I am not sure what time zone the app runs in, so 6:01 is a hedge. If it's ET, it happens before start of business. If PT, it happens right at start of business. If it's UTC, it happens after midnight ET, so the last month's measurements will be in.

--- internal/jobs/client.go
+++ internal/jobs/client.go
@@ -9,21 +9,28 @@ import (
 	"github.com/cloud-gov/billing/internal/dbx"
 	"github.com/cloud-gov/billing/internal/usage/reader"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/riverqueue/river"
 	"github.com/riverqueue/river/riverdriver/riverpgxv5"
 	"github.com/robfig/cron/v3"
 )
 
+// NewClient creates a new river client with periodic jobs scheduled.
 func NewClient(conn *pgxpool.Pool, logger *slog.Logger, q dbx.Querier, rdr *reader.Reader) (*river.Client[pgx.Tx], error) {
 	workers := river.NewWorkers()
 	river.AddWorker(workers, NewMeasureUsageWorker(logger, conn, q, rdr))
 
-	schedule, err := cron.ParseStandard("1 * * * *") // Read usage every hour, one minute after the hour.
+	measureUsageSchedule, err := cron.ParseStandard("1 * * * *") // Read usage every hour, one minute after the hour.
 	if err != nil {
-		return nil, fmt.Errorf("parsing cron spec: %w", err)
+		return nil, fmt.Errorf("parsing measureUsage cron spec: %w", err)
 	}
-	riverc, err := river.NewClient(riverpgxv5.New(conn), &river.Config{
+	postUsageSchedule, err := cron.ParseStandard("1 6 1 * *") // Post usage on the first of every month at 6:01am.
+	if err != nil {
+		return nil, fmt.Errorf("parsing postUsage cron spec: %w", err)
+	}
+
+	return river.NewClient(riverpgxv5.New(conn), &river.Config{
 		JobTimeout: 10 * time.Minute,
 		Logger:     logger,
 		Queues: map[string]river.QueueConfig{
@@ -31,7 +38,7 @@ func NewClient(conn *pgxpool.Pool, logger *slog.Logger, q dbx.Querier, rdr *read
 		},
 		PeriodicJobs: []*river.PeriodicJob{
 			river.NewPeriodicJob(
-				schedule,
+				measureUsageSchedule,
 				func() (river.JobArgs, *river.InsertOpts) {
 					return MeasureUsageArgs{
 						Periodic: true,
@@ -39,8 +46,17 @@ func NewClient(conn *pgxpool.Pool, logger *slog.Logger, q dbx.Querier, rdr *read
 				},
 				nil,
 			),
+			river.NewPeriodicJob(
+				postUsageSchedule,
+				func() (river.JobArgs, *river.InsertOpts) {
+					return PostUsageArgs{
+						Periodic: true,
+						AsOf:     pgtype.Timestamptz{Time: time.Now().UTC(), Valid: true},
+					}, nil
+				},
+				nil,
+			),
 		},
 		Workers: workers,
 	})
-	return riverc, err
 }
