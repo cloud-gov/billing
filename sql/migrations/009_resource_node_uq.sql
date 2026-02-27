99b7a28b7090c5b2c7546b0174e3659292c7d520
Zach Rollyson <zachary.rollyson@gsa.gov>
2026-02-27 09:34:45 -0500

    fix: actually, path isn't even a useful constraint
    E.g., apps get deleted and re-created with the same name; they aren't
    literally the same anymore, but I don't have a compelling reason to
    think the path should change.

--- sql/migrations/009_resource_node_uq.sql
+++ sql/migrations/009_resource_node_uq.sql
@@ -1,9 +1,7 @@
 alter table resource_node
-drop constraint resource_node_slug_uq,
-add constraint resource_node_path_uq unique (customer_id, path);
+drop constraint resource_node_slug_uq;
 
 ---- create above / drop below ----
 
 alter table resource_node
-drop constraint resource_node_path_uq,
 add constraint resource_node_slug_uq unique (customer_id, slug);
